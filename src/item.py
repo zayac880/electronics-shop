c6a1cc8cce2a4454210e38196eacbeb98c85c040
Alex <zayac880880@mail.ru>
2023-05-04 21:32:06 +0300

    1

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int):
         """
         Создание экземпляра класса item.
 
@@ -18,7 +21,19 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
-    def calculate_total_price(self) -> float:
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        try:
+            assert len(value) <= 10
+            self._name = value
+        except AssertionError:
+            pass
+
+    def calculate_total_price(self):
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
@@ -26,11 +41,29 @@ class Item:
         """
         return self.price * self.quantity
 
-    def apply_discount(self) -> None:
+    def apply_discount(self):
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('C:/Users/Alex/Desktop/OOP/electronics-shop/src/items.csv') as f:
+            reader = csv.DictReader(f)
+            items = []
+            for row in reader:
+                name = row['name']
+                price = cls.string_to_number(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all.clear()
+            cls.all.extend(list(items))
 
-
+    @staticmethod
+    def string_to_number(value: str):
+        try:
+            return int(value)
+        except ValueError:
+            return int(float(value))
