dd85dd6b3a59c334a252f7d85a46ca9365986ead
Alex <zayac880880@mail.ru>
2023-05-09 19:39:13 +0300

    homework3

--- src/item.py
+++ src/item.py
@@ -21,49 +21,60 @@ class Item:
         self.quantity = quantity
         self.all.append(self)
 
-    @property
-    def name(self):
-        return self._name
 
-    @name.setter
-    def name(self, value: str):
-        try:
-            assert len(value) <= 10
-            self._name = value
-        except AssertionError:
-            pass
+    def __repr__ (self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-    def calculate_total_price(self):
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+    def __str__(self):
+        return f'{self.name}'
 
-        :return: Общая стоимость товара.
-        """
-        return self.price * self.quantity
 
-    def apply_discount(self):
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        self.price *= self.pay_rate
 
-    @classmethod
-    def instantiate_from_csv(cls):
-        with open('C:/Users/Alex/Desktop/OOP/electronics-shop/src/items.csv') as f:
-            reader = csv.DictReader(f)
-            items = []
-            for row in reader:
-                name = row['name']
-                price = cls.string_to_number(row['price'])
-                quantity = int(row['quantity'])
-                item = cls(name, price, quantity)
-                items.append(item)
-            cls.all.clear()
-            cls.all.extend(list(items))
 
-    @staticmethod
-    def string_to_number(value: str):
-        try:
-            return int(value)
-        except ValueError:
-            return int(float(value))
+#    @property
+#   def name(self):
+#      return self._name
+
+#   @name.setter
+#   def name(self, value: str):
+#        try:
+#            assert len(value) <= 10
+#            self._name = value
+#        except AssertionError:
+#            pass
+
+#    def calculate_total_price(self):
+#        """
+#       Рассчитывает общую стоимость конкретного товара в магазине.
+#
+#        :return: Общая стоимость товара.
+#        """
+#        return self.price * self.quantity
+
+#    def apply_discount(self):
+#        """
+#        Применяет установленную скидку для конкретного товара.
+#        """
+#        self.price *= self.pay_rate
+
+#    @classmethod
+#    def instantiate_from_csv(cls):
+#        with open('C:/Users/Alex/Desktop/OOP/electronics-shop/src/items.csv') as f:
+#            reader = csv.DictReader(f)
+#            items = []
+#            for row in reader:
+#                name = row['name']
+#                price = cls.string_to_number(row['price'])
+#                quantity = int(row['quantity'])
+#                item = cls(name, price, quantity)
+#                items.append(item)
+#            cls.all.clear()
+#            cls.all.extend(list(items))
+
+#    @staticmethod
+#    def string_to_number(value: str):
+#        try:
+#            return int(value)
+#        except ValueError:
+#            return int(float(value))
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,25 +1,34 @@
 from src.item import Item
 
 
-def test_calculate_total_price():
-    item = Item("test item", 10, 2)
-    assert item.calculate_total_price() == 20
+#def test_calculate_total_price():
+#    item = Item("test item", 10, 2)
+#    assert item.calculate_total_price() == 20
 
 
-def test_apply_discount():
-    Item.pay_rate = 0.9
-    item = Item("test item", 10, 2)
-    item.apply_discount()
-    assert item.price == 9.0
-    assert item.calculate_total_price() == 18
+#def test_apply_discount():
+#    Item.pay_rate = 0.9
+#    item = Item("test item", 10, 2)
+#    item.apply_discount()
+#    assert item.price == 9.0
+#    assert item.calculate_total_price() == 18
 
 
-def test_set_name():
-    item = Item("test item", 10, 2)
-    item.name = "new name"
-    assert item.name == "new name"
+#def test_set_name():
+#    item = Item("test item", 10, 2)
+#    item.name = "new name"
+#    assert item.name == "new name"
 
 
-def test_string_to_number():
-    assert Item.string_to_number('10') == 10
-    assert Item.string_to_number('20.5') == 20
+#def test_string_to_number():
+#   assert Item.string_to_number('10') == 10
+#    assert Item.string_to_number('20.5') == 20
+
+
+def test_item_repr():
+    item = Item("test item", 10.99, 5)
+    assert repr(item) == "Item('test item', 10.99, 5)"
+
+def test_item_str():
+    item = Item("test item", 10.99, 5)
+    assert str(item) == "test item"
