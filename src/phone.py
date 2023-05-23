00285485060283d0500754ee4846ee10623c0fe1
Alex <zayac880880@mail.ru>
2023-05-23 14:23:46 +0300

    homework4

--- src/phone.py
+++ src/phone.py
@@ -9,7 +9,17 @@ class Phone(Item):
         self.number_of_sim = number_of_sim
         self.all.append(self)
 
-    def __repr__ (self):
+    @property
+    def number_of_sim(self):
+        return self._number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        self._number_of_sim = value
+
+    def __repr__(self):
         return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     def __str__(self):
